09dd9eb8f76507a4d079dfd8fe02609017831922
Oleksandr Herashchenko <s238155@studenti.polito.it>
2019-04-09 13:26:15 +0200

    lezione 09.04

--- src/it/polito/tdp/model/Model.java
+++ src/it/polito/tdp/model/Model.java
@@ -1,14 +1,91 @@
 package it.polito.tdp.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Model {
-
+	
+	// esami letti dal database
+	private List<Esame> esami;
+	
+	// gestione della ricorsione
+	private List<Esame> best;
+	private double media_best;
+	
+	/**
+	 * Trova la combinazione di corsi avente la somma di crediti che abbia la media dei voti massima
+	 * @param numeroCrediti
+	 * @return l'elenco dei corsi ottimale, oppure {@code null} alcuna combinazione di corsi che assomma al numero esatto di crediti.
+	 */
 	public List<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
 		
-		System.out.println("TODO!");
+		best = null;
+		media_best = 0.0;
+		Set<Esame> parziale = new HashSet<Esame>();
 		
-		return null;
+		cerca(parziale, 0, numeroCrediti);
+		
+		return best;
+	}
+	
+	private void cerca(Set<Esame> parziale, int L, int m) {
+		
+		// casi terminali
+		int crediti = sommaCrediti(parziale);
+		
+		if (crediti > m)
+			return ;
+		
+		if (crediti == m) {
+			double media = calcolaMedia(parziale);
+			
+			if (media > media_best) {
+				best = new ArrayList<Esame>(parziale);
+				media_best = media; 
+				return ;
+			} else 
+				return ;
+			
+		}
+		
+		// di sicuro crediti < m
+		if (L == esami.size())
+			return ;
+		
+		
+	/*     generiamo sotto-problemi
+		   esami[L] è da aggiungere o no?
+		   provo a non aggiungerlo          */
+		cerca(parziale, L+1, m);
+		
+		// provo ad aggiungerlo
+		parziale.add(esami.get(L));
+		cerca(parziale, L+1, m);
+		parziale.remove(esami.get(L));
+			
+	}
+
+	private int sommaCrediti(Set<Esame> parziale) {
+		int somma = 0;
+		
+		for (Esame e: parziale)
+			somma += e.getCrediti();
+			
+		return somma;
+	}
+
+	private double calcolaMedia(Set<Esame> parziale) {
+		double media = 0.0;
+		int crediti = 0;
+		
+		for (Esame e: parziale) {
+			media += e.getCrediti()*e.getVoto();
+			crediti += e.getCrediti();
+		}
+		
+		return media/crediti;
 	}
 
 }
